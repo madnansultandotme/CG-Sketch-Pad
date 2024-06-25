c8ac05cc3e2a67af7da49174c90e32952d4cf60c
Muhammad Adnan Sultan <info.adnansultan@gmail.com>
2024-06-25 23:24:30 +0500

    Added project from local to Remote

---
+++ Sketch Pad with Top Menu/Resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Sketch Pad with Top Menu.rc
+
+#define IDM_DRAW_POINT          1001
+#define IDM_DRAW_LINE           1002
+#define IDM_DRAW_POLYLINE       1003
+#define IDM_DRAW_POLYGON        1004
+#define IDM_DRAW_RECTANGLE      1005
+#define IDM_INCREASE_WIDTH      1006
+#define IDM_DECREASE_WIDTH      1007
+#define IDM_INCREASE_SIZE       1008
+#define IDM_DECREASE_SIZE       1009
+#define IDM_SET_BG_COLOR        1010
+#define IDM_SET_DRAW_COLOR      1011
+#define IDM_ERASE_LAST          1012
+#define IDM_CLEAR_ALL           1013
